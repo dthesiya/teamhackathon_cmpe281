496e693f004b970e7ca161dade5a0a6d6686ff77
TanmayAB <tabhatt1993@gmail.com>
2017-04-03 20:42:33 -0700

    Adding connection with Cassandra

--- RESTBUCKS_NODE_API_TANMAY/NodeServer_T/app.js
+++ RESTBUCKS_NODE_API_TANMAY/NodeServer_T/app.js
@@ -1,10 +1,9 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-var apis = require('./routes/apis')
+var apis = require('./routes/apis');
 //app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 //app.use(logger('dev'));
@@ -20,7 +19,7 @@ var apis = require('./routes/apis')
 app.use('/',apis);
 //var database = require('./database/db.js');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000); 
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
